6b5322f28fc125bcce61dd50f3c76c0876643d6b
CyclonaGowen <cgowen@dialecticapps.com>
2021-08-16 15:19:33 -0500

    added menu items, price, desc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { useState } from 'react';
-import Menu from './Menu';
+// components to add
+import Menu from './Menu'; 
 import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setItemsMenu] = useState(items);
+  const [categories, setCategores] = useState([]);
+
+  return <main>
+    <section className="menu section">
+      <div className="title">
+        <h2>Our Menu</h2>
+        <div className="underline"></div>
+      </div>
+      <Categories />
+      <Menu items={menuItems}/>
+    </section>
+  </main>;
 }
 
 export default App;
